0638b36acd7d4f9a0355f985c04a728c0e9c5e29
Keyshawnas <keyshawnasingleton@gmail.com>
2023-10-16 00:19:17 -0400

    added icon link

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function showTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/10d@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 let h1 = document.querySelector("#city");
